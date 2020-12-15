dffeef53606cc82111925fae729570af3f9de900
James Hulme <james.h@38degrees.org.uk>
2020-12-15 21:57:22 +0000

    Optimised day 15 a bit
    Results returned faster, and number building massive arrays

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,32 +1,32 @@
 last_spoken_when = {}
-numbers = []
+last_said = -1
 
 def first_time(last_spoken):
-    return len(last_spoken_when[last_spoken]) <= 1
+    return last_spoken_when[last_spoken][0] is None
 
 def say(i, number):
+    global last_said
     if number in last_spoken_when:
-        last_spoken_when[number].append(i)
+        last_spoken_when[number] = (last_spoken_when[number][1], i)
     else:
-        last_spoken_when[number] = [i]
+        last_spoken_when[number] = (None, i)
 
-    numbers.append(number)
+    last_said = number
 
 def part1(seed, goal):
     for i, number in enumerate(seed):
         say(i, number)
 
     for i in range(len(seed), goal):
-        last_spoken = numbers[i - 1]
-        if first_time(last_spoken):
+        if first_time(last_said):
             say(i, 0)
         else:
-            last_spoken_at = last_spoken_when[last_spoken][-2:]
+            last_spoken_at = last_spoken_when[last_said]
             say(i, last_spoken_at[1] - last_spoken_at[0])
 
-    return numbers[-1]
+    return last_said
 
 print(part1([0,13,16,17,1,10,6], 2020))
 last_spoken_when = {}
-numbers = []
+last_said = -1
 print(part1([0,13,16,17,1,10,6], 30000000))
