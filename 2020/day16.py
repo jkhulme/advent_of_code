553e8a14da32dd20249c2fcd2b5c86c55444278e
James Hulme <james.h@38degrees.org.uk>
2020-12-18 16:32:22 +0000

    Day 16
    Two gold stars

--- 2020/day16.py
+++ 2020/day16.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 
 
 def parse_input():
@@ -46,8 +47,31 @@ def valid_ticket_section(rules, ticket_section):
 def invalid_ticket_sections(rules, ticket):
     return [ticket_section for ticket_section in ticket if not valid_ticket_section(rules, ticket_section)]
 
-def flatten(rules):
-    return rules
+def find_ticket_section(tickets, rules):
+    possible_sections = {}
+
+    for i in range(0, len(tickets[0])):
+        possible_sections[i] = []
+        ticket_section = [ticket[i] for ticket in tickets]
+
+        for label, rule in rules.items():
+            if all([valid_ticket_section(rule, t) for t in ticket_section]):
+                possible_sections[i].append(label)
+
+    return possible_sections
+
+def ticket_csp_solver(ticket_sections):
+    known = {}
+    while len(ticket_sections.keys()) != len(known.keys()):
+        for i, sections in ticket_sections.items():
+            if i in known:
+                continue
+
+            possible = [section for section in sections if section not in known.values()]
+            if len(possible) == 1:
+                known[i] = possible[0]
+
+    return known
 
 def part1():
     rules, your_ticket, nearby_tickets = parse_input()
@@ -56,8 +80,11 @@ def part1():
 
 def part2():
     rules, your_ticket, nearby_tickets = parse_input()
-    # valid_tickets = [ticket for ticket in nearby_tickets if len(invalid_ticket_sections(rules, ticket)) == 0]
-    # return valid_tickets
+    flat_rules = list(itertools.chain.from_iterable(rules.values()))
+    valid_tickets = [ticket for ticket in nearby_tickets if len(invalid_ticket_sections(flat_rules, ticket)) == 0]
+    ticket_sections = ticket_csp_solver(find_ticket_section(valid_tickets, rules))
+    sections = [i for i, section in ticket_sections.items() if section.startswith('departure')]
+    return math.prod([your_ticket[i] for i in sections])
 
 print(part1())
 print(part2())
