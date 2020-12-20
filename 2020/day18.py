d8470f4440139779e6ae7232e77b30f7790296dc
James Hulme <james.h@38degrees.org.uk>
2020-12-20 18:41:50 +0000

    Day 18
    Save point 2
    Handles some nested brackets, but not all.

--- 2020/day18.py
+++ 2020/day18.py
@@ -1,25 +1,36 @@
 def parse_input():
     return [
-        '2 * 3 + (4 * 5)',
-        '5 + (8 * 3 + 9 + 3 * 4 * 3)',
-        '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))'
+        # '2 * 3 + (4 * 5)',
+        # '5 + (8 * 3 + 9 + 3 * 4 * 3)',
+        # '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))',
+        '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2'
     ]
 
 def evaluate(expression):
+    print(expression)
+    has_brackets = False
     for i, e in enumerate(expression):
         if e.startswith('('):
+            has_brackets = True
             start = i
         if e.endswith(')'):
             end = i
+            opening = expression[start].count('(')
+            closing = expression[end].count(')')
             expression[start] = expression[start].replace('(', '')
             expression[end] = expression[end].replace(')', '')
-            expression[start:end + 1] = [sub_evaluate(expression[start:end + 1])]
+            expression[start:end + 1] = ['(' * (opening - 1) + str(evaluate(expression[start:end + 1])) + ')' * (closing - 1)]
 
-    print(expression)
+    if has_brackets:
+        expression = evaluate(expression)
 
     return sub_evaluate(expression)
 
 def sub_evaluate(expression):
+    # Not sure why I need this case
+    if isinstance(expression, int):
+        return expression
+
     if len(expression) == 1:
         return int(expression[0])
 
