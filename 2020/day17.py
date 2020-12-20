bce76e956fd0c6df6efa3bb03fe254501eb3d0c1
James Hulme <james.h@38degrees.org.uk>
2020-12-20 12:22:10 +0000

    Day 17
    1 Gold star

--- 2020/day17.py
+++ 2020/day17.py
@@ -32,9 +32,81 @@ def expand_cube(cube):
 
     return new_cube
 
+def neighbours(x, y, z, cube):
+    max_z = len(cube)
+    max_y = len(cube[0])
+    max_x = len(cube[0][0])
+
+    adjacent = [
+        (x - 1, y - 1, z - 1),
+        (x, y - 1, z - 1),
+        (x + 1 , y - 1, z - 1),
+        (x - 1, y, z - 1),
+        (x, y, z - 1),
+        (x + 1, y, z - 1),
+        (x - 1, y + 1, z - 1),
+        (x, y + 1, z - 1),
+        (x + 1, y + 1, z - 1),
+        (x - 1, y - 1, z),
+        (x, y - 1, z),
+        (x + 1 , y - 1, z),
+        (x - 1, y, z),
+        (x + 1, y, z),
+        (x - 1, y + 1, z),
+        (x, y + 1, z),
+        (x + 1, y + 1, z),
+        (x - 1, y - 1, z + 1),
+        (x, y - 1, z + 1),
+        (x + 1 , y - 1, z + 1),
+        (x - 1, y, z + 1),
+        (x, y, z + 1),
+        (x + 1, y, z + 1),
+        (x - 1, y + 1, z + 1),
+        (x, y + 1, z + 1),
+        (x + 1, y + 1, z + 1),
+    ]
+
+    return [(x, y, z) for x, y, z in adjacent if x >= 0 and y >= 0 and z >= 0 and x < max_x and y < max_y and z < max_z]
+
+def new_state(x, y, z, cube):
+    current_state = cube[z][y][x]
+    active_neighbours = sum([1 for n in neighbours(x, y, z, cube) if cube[n[2]][n[1]][n[0]] == '#'])
+
+    if current_state == '#' and (active_neighbours == 2 or active_neighbours == 3):
+        return '#'
+    elif current_state == '.' and active_neighbours == 3:
+        return '#'
+    else:
+        return '.'
+
+def simulate(cube):
+    cube = expand_cube(cube)
+    new_cube = []
+    for z, layer in enumerate(cube):
+        new_layer = []
+        for y, row in enumerate(layer):
+            new_row = []
+            for x, cell in enumerate(row):
+                new_row.append(new_state(x, y, z, cube))
+            new_layer.append(new_row)
+        new_cube.append(new_layer)
+
+    return new_cube
+
+def count_active(cube):
+    total = 0
+    for layer in cube:
+        for row in layer:
+            for cell in row:
+                if cell == '#':
+                    total += 1
+
+    return total
+
 def part1():
     cube = [parse_input()]
-    return print_cube(expand_cube(cube))
-
+    for i in range(0, 6):
+        cube = simulate(cube)
+    return count_active(cube)
 
 print(part1())
